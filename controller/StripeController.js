26109f5ceb5a31e18216f4d34cb6f19cda2e174b
muhammmadmahad-debug <mahadhashmi2@gmail.com>
2023-05-26 09:22:07 +0500

    fix

--- controller/StripeController.js
+++ controller/StripeController.js
@@ -4,12 +4,13 @@ import  ReservationSchema  from '../models/Reservations.js'
 const stripeInstance = stripe('sk_test_51H64odEGhJpVZtxXTBVcvT34NrAZp0A5x5gdA32IAgN6Ac6adoKrOPMqiKaaRmf0sQ8iUvFVbIIvjqn1UXjsixTC00hWhpxTvu');
 export const createPayment = async (req, res, next) => {
  const {token , date , chargeBoxId } = req.body; // Token received from the client-side Stripe Checkout component
+ console.log("🚀 ~ file: StripeController.js:7 ~ createPayment ~ chargeBoxId:", chargeBoxId)
 
   try {
      await stripeInstance.charges.create({
       amount: 1000, // Amount in cents
       currency: 'USD',
-      source: token,
+      source: token.id,
       description: 'Thanks for the Reservation'
     });
             const reserve = new ReservationSchema({
@@ -19,6 +20,7 @@ export const createPayment = async (req, res, next) => {
 
     res.status(200).json({ message: 'Charge created successfully' });
   } catch (error) {
+    console.log("🚀 ~ file: StripeController.js:22 ~ createPayment ~ error:", error)
     res.status(500).json({ error: 'Error creating charge' });
   }
 
