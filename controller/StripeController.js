6f50b3417d83615d98c5315a004d2a2708e0d34a
muhammmadmahad-debug <mahadhashmi2@gmail.com>
2023-05-26 09:06:33 +0500

    fix

--- controller/StripeController.js
+++ controller/StripeController.js
@@ -1,12 +1,12 @@
-const stripe = require('stripe')('sk_test_51H64odEGhJpVZtxXTBVcvT34NrAZp0A5x5gdA32IAgN6Ac6adoKrOPMqiKaaRmf0sQ8iUvFVbIIvjqn1UXjsixTC00hWhpxTvu');
+import stripe from 'stripe';
+import  ReservationSchema  from '../models/Reservations.js' 
 
-const ReservationSchema  = require( '../models/Reservations');
-
+const stripeInstance = stripe('sk_test_51H64odEGhJpVZtxXTBVcvT34NrAZp0A5x5gdA32IAgN6Ac6adoKrOPMqiKaaRmf0sQ8iUvFVbIIvjqn1UXjsixTC00hWhpxTvu');
 export const createPayment = async (req, res, next) => {
  const {token , date , chargeBoxId } = req.body; // Token received from the client-side Stripe Checkout component
 
   try {
-     await stripe.charges.create({
+     await stripeInstance.charges.create({
       amount: 1000, // Amount in cents
       currency: 'USD',
       source: token,
--- routes/stripeRouter.js
+++ routes/stripeRouter.js
@@ -3,7 +3,7 @@
 import express from "express";
 import {
  createPayment
-} from "../controller/StripeController";
+} from "../controller/StripeController.js";
 
 const router = express.Router();
 
